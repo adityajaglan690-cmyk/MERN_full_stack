ae339745dc06880f21026bbdb8757760ab69bdc0
Aditya jaglan <adityajaglan690@gmail.com>
2026-07-13 19:16:46 +0530

    13/7/26

---
+++ js/digital clock.js
@@ -0,0 +1,20 @@
+let div=document.createElement("div");
+let h1=document.createElement("h1");
+let h2=document.createElement("h2");
+
+function clock(){
+    let time=new Date();
+    // console.log(time);
+    let hrs=String(time.getHours()).padStart(2, '0');
+    let min=String(time.getMinutes()).padStart(2, '0');
+    let sec=String(time.getSeconds()).padStart(2, '0');
+    console.log(`${hrs} ${min} ${sec}`);
+    h1.innerHTML=`${hrs}:${min}:${sec}`
+     h2.innerHTML=`${hrs}:${min}:${sec}`
+}
+div.appendChild(h1);
+document.body.appendChild(div);
+div.appendChild(h2);
+document.body.appendChild(div);
+clock();
+setInterval(clock,1000);
---
+++ js/setinterval.js
@@ -0,0 +1,12 @@
+setInterval(() => {
+    console.log("Hello motu");
+}, 1000);
+
+
+
+setTimeout(() => {
+    console.log("me hu aditya");
+}, 1000);
+
+
+// event looping  && event emiiter
