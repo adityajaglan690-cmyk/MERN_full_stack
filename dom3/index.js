016f4a418cb8c12e2393e94ecbddcf4873d96670
Aditya jaglan <adityajaglan690@gmail.com>
2026-07-16 16:51:45 +0530

    16/7/2026

--- dom3/index.js
+++ dom3/index.js
@@ -1,12 +1,36 @@
 let task=document.getElementById("task");
-console.log(task);
-let list=document.getElementById("list");
+// console.log(task);
+// function handleclick(
+let list=document.getElementById("list")
 
-
+    
 document.getElementById("btn").addEventListener('click',()=>{
-    console.log(task);
-    let li=document.createElement("li");
-    li.innerHTML=~`${task.value}`;
-    list.appendChild(li);
+    // console.log(task.value);
+    if(task.value==""){
+        alert("enter any task before add new task")
+        return;
+    }else{
+          let li=document.createElement("li")
+          li.innerHTML=`${task.value}`
+
+          let complete=document.createElement("button");
+          complete.innerHTML="complete";
+          complete.addEventListener("click",()=>{
+        //  js text decoration syntax
+            li.style.textDecoration="line-through";
+          })
+
+           let dlt=document.createElement("button");
+          dlt.innerHTML="Delete";
+
+          dlt.addEventListener("click",()=>{
+            li.remove();
+          })
+
+          li.appendChild(complete)
+          li.appendChild(dlt)
+         list.appendChild(li)
+         task.value="";
+    }
 
 })
---
+++ js/operators.js
@@ -0,0 +1,117 @@
+// artimetic operators
+// let a =5;
+// let b =2;
+// console.log("a+b=",a+b);
+// console.log("a-b=",a-b);
+// console.log("a=",a,"&b=",b);
+// console.log("a*b=",a*b);
+// console.log("a/b=",a/b);
+// console.log("a%b=",a%b);
+// console.log("a**b=",a**b);
+
+
+
+// unary operator
+// let a =5;
+// let b =2;
+// console.log("a=",a,"&b=",b); 
+
+// console.log("++a=",++a);
+// console.log("a++=",a++);
+// console.log("--a=",--a);
+
+
+
+// asignment operator
+// let a =5;
+// let b =2;
+// // a+=4; //a=a+4
+// // a-=4; //a=a-4
+// // a/=4; //a=a/4
+// a*=4; //a=a*4
+// console.log("a=",a);//9
+
+
+
+
+// comparison operator   
+// let a =5;
+// let b =5;
+
+// console.log("5==5",a==b);
+// console.log("5>=5",a>=b);
+
+
+
+// logical operator      condition ko check krta  h
+// let a=6;
+// let b=5;
+
+// let cond1=a>b;//true
+// let cond2=a===6;//true
+// console.log("cond1&&cond2=",cond1&&cond2);
+
+
+
+// conditional statement
+
+// let age =16;
+// if (age>=18){
+//     console.log("you can vote");
+    
+// }
+// if (age <18){
+//     console.log("you cannot vote");
+    
+// }
+
+
+// let mode ="dark"
+// let color;
+
+// if(mode==="dark"){
+//     color ="black";
+// }
+// if(mode==="light"){
+//     color ="white";
+// }
+// console.log(color);
+
+
+
+
+// let mode ="dark"
+// let color;
+
+// if(mode==="dark"){
+//     color ="black";
+// }
+// else{
+//     color ="white";
+// }
+// console.log(color);
+
+
+
+
+// let age =25;
+
+// if(age>=18){
+//     console.log("vote");
+    
+// }else{
+//     console.log("not vote");
+    
+// }
+
+
+// odd or even
+let num =10;
+
+if (num%2 ===0){
+    console.log(num,"is even ");
+    
+}else{
+    console.log(num,"is odd");
+    
+}
